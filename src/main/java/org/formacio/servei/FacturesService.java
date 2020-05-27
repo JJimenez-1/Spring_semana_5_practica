2b565452abfcdfe0d49b90fe94da8ea286450224
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-05-27 19:41:46 +0200

    refactor(FacturesService): ahora es un component

--- src/main/java/org/formacio/servei/FacturesService.java
+++ src/main/java/org/formacio/servei/FacturesService.java
@@ -1,9 +1,25 @@
 package org.formacio.servei;
 
-import org.formacio.domain.Factura;
+import java.util.Optional;
 
+
+
+import org.formacio.domain.Factura;
+import org.formacio.domain.LiniaFactura;
+import org.formacio.repositori.FacturesRepositori;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class FacturesService {
 
+	@Autowired
+	FacturesRepositori repoFactures;
+	
+	@Autowired
+	FidalitzacioService fidelitzacioServei;
 	
 	/*
 	 * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
@@ -14,7 +30,24 @@ public class FacturesService {
 	 * Per implementar aquest metode necessitareu una referencia (dependencia) a FacturesRepositori
 	 */
 	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
+		Optional<Factura> factura = repoFactures.findById(idFactura);
 		
-		return null;
+		if(factura.isPresent()){
+			LiniaFactura linia = new LiniaFactura();
+			linia.setProducte(producte);
+			linia.setTotal(totalProducte);
+			factura.get().getLinies().add(linia);
+			repoFactures.save(factura.get());
+			
+			notificaPremi(factura.get());
+		}
+		return factura.get();
+	}
+	
+	public void notificaPremi(Factura factura){
+		final int NUM_LINIES_AMB_PREMI = 4;
+		if(factura.getLinies().size() >= NUM_LINIES_AMB_PREMI){
+			fidelitzacioServei.notificaRegal(factura.getClient().getEmail());
+		}
 	}
 }
