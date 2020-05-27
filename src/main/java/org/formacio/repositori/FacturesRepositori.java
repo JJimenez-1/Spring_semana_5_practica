6889d27d23e03e35aff93a26f28df0fad0f54f87
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-05-27 19:42:41 +0200

    feat(FacturesRepositori): añadir nueva clase afegirProducte

--- src/main/java/org/formacio/repositori/FacturesRepositori.java
+++ src/main/java/org/formacio/repositori/FacturesRepositori.java
@@ -1,8 +1,15 @@
 package org.formacio.repositori;
 
+import org.formacio.domain.Factura;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface FacturesRepositori  {
+public interface FacturesRepositori extends CrudRepository<Factura, Long>{
 
+	@Query("select sum(linia.total) from Factura factura join factura.linies linia where factura.client.nom = ?1")
 	public Number totalClient(String client);
+	
+	public List<Factura> findByClientNom(String nom);
 	
 }
